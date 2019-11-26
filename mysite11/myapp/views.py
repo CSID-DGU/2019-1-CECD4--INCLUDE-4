801b9159e5dea1466d5293ea3dec5d5d7c1d5e41
yoonsy97 <43856086+yoonsy97@users.noreply.github.com>
2019-11-27 01:49:06 +0900

    update signup

--- mysite11/myapp/views.py
+++ mysite11/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import SignupForm,SigninForm
+from .forms import SigninForm
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.http.response import HttpResponseRedirect
@@ -222,39 +222,30 @@ def list(request):
     context= {'company':company,'posts':posts}
     return render(request, 'myapp/list.html',context)
 
-def signup(request):#역시 GET/POST 방식을 사용하여 구현한다.
+def signup(request):
     if request.method == "GET":
-        return render(request, 'myapp/signup.html', {'f':SignupForm()} )
+        return render(request, 'myapp/signup.html' )
     
     
     elif request.method == "POST":
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            if form.cleaned_data['password']  == form.cleaned_data['password_check']:
-#cleaned_data는 사용자가 입력한 데이터를 뜻한다.
-#즉 사용자가 입력한 password와 password_check가 맞는지 확인하기위해 작성해주었다.
+      
+            if request.POST['password']==request.POST['password_check']:
+           
                 code=str(User.objects.count()).zfill(6)
-                new_user = User.objects.create_user(form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
+                new_user=User.objects.create_user(username=request.POST['username'],
+                password=request.POST['password'],
                 client_code=code,
-                MetamastAddress=form.cleaned_data['metamask'])
-#User.object.create_user는 사용자가 입력한 name, email, password를 가지고 아이디를 만든다.
-#바로 .save를 안해주는 이유는 User.object.create를 먼저 해주어야 비밀번호가 암호화되어 저장된다.
+                MetamastAddress=request.POST['metamask'])
+
                
-            #    new_user.last_name = form.cleaned_data['last_name']
-            #   new_user.first_name = form.cleaned_data['first_name']
-#나머지 입력하지 못한 last_name과, first_name은 따로 지정해준다.
                 new_user.save()
-                
                 return render(request,'myapp/home.html')      
             else:
-                return render(request, 'myapp/signup.html',{'f':form, 'error':'비밀번호와 비밀번호 확인이 다릅니다.'})#password와 password_check가 다를 것을 대비하여 error를 지정해준다.
+                return render(request, 'myapp/signup.html',{ 'error':'비밀번호와 비밀번호 확인이 다릅니다.'})#password와 password_check가 다를 것을 대비하여 error를 지정해준다.
 
-        else: #form.is_valid()가 아닐 경우, 즉 유효한 값이 들어오지 않았을 경우는
+    else:
+        return render(request, 'myapp/signup.html')
 
-            return render(request, 'myapp/signup.html',{'f':form})
-#원래는 error 메시지를 지정해줘야 하지만 따로 지정해주지 않는다.
-#그 이유는 User 모델 클래스에서 자동으로 error 메시지를 넘ㄱ
 
 
 def statechange(request,tokenname):
